a458aafc42157c3d9ce955b68ceacdcc4caaae47
Hinako Terado <aa123124@gs.gsc.aoyama.ac.jp>
2025-11-16 14:14:33 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const map = new maplibregl.Map({
   container: "map",
-  center: [139.507551, 35.603615], // 中心座標
-  zoom: 10, // ズームレベル
+  center: [139.507442, 35.603379], // 中心座標
+  zoom: 16, // ズームレベル
   style: {
     // スタイル仕様のバージョン番号。8を指定する
     version: 8,
